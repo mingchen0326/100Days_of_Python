00f9f7a330ab5d3b1751f6a2a5705eaa584facef
Ming Chen <mingchen0321@gmail.com>
2022-08-02 10:48:06 -0400

    add error capture feature

--- Day26/main.py
+++ Day26/main.py
@@ -6,6 +6,15 @@ nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(nato_dict)
 
 # generate nato code based on user input
-name = input("Please enter your name: ").upper()
-user_nato = {letter: nato_dict[letter] for letter in name}
-print(user_nato)
+def generate_phonetics():
+    name = input("Please enter your name: ").upper()
+    try:
+        user_nato = {letter: nato_dict[letter] for letter in name}
+    except KeyError:
+        print("Sorry, only letter in the alphabet please")
+        generate_phonetics()
+    else:
+        print(user_nato)
+
+
+generate_phonetics()
