5eb14087721c8be99a72c5e2f8d721ef6982e41d
Ming Chen <mingchen0321@gmail.com>
2022-08-02 12:18:01 -0400

    add error capture and password search feature

--- Day29/main.py
+++ Day29/main.py
@@ -1,6 +1,7 @@
 import string
 import tkinter
 import random
+import json
 from tkinter import messagebox
 
 """Build a password manager"""
@@ -44,69 +45,54 @@ def add():
     # get the text content from website and username entry box
     website = entry_website.get()
     username = entry_username.get()
+    new_data = {
+        website: {
+            "username": username,
+            "password": password,
+        }
+    }
 
     # check empty input
     if website == "" or username == "" or password == "":
         messagebox.showinfo(title="Blank Input", message="Entry cannot be empty")
-        # -------------------------------the following code is to create the message box using canvas-------------------
-        # blank_top = tkinter.Toplevel(window)
-        # blank_top.geometry("300x300")
-        # blank_top.title("Empty Blank")
-        #
-        # blank_message = tkinter.Label(blank_top, text="Entry cannot be empty")
-        # blank_message.grid(row=1, column=1)
-        #
-        # def exit_blank_top():
-        #     blank_top.destroy()
-        #     blank_top.update()
-        #
-        # # OK button to exit the pop-up window
-        # ok_button = tkinter.Button(blank_top, text="OK", command=exit_blank_top)
-        # ok_button.grid(row=2, column=2)
 
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"Website: {website}\n"
-                                                              f"Email: {username}\n"
-                                                              f"Password: {password}\n"
-                                                              f"Is this ok?")
-        if is_ok:
-            file = open("data.txt", 'a+')
-            file.write(f"{website}  ||  {username} || {password}\n")
+        try:
+            with open("data.json", "r") as data_file:
+                # Reloading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                # Reloading old data
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             entry_website.delete(0, "end")
             entry_username.delete(0, "end")
             entry_password.delete(0, "end")
-            file.close()
-        # -------------------------------the following code is to create the pop-up box using canvas------------------
-        # top = tkinter.Toplevel(window)
-        # top.geometry("400x400")
-        # top.title("Confirmation")
-        #
-        # # create image in canvas
-        # canvas2 = tkinter.Canvas(top, width=100, height=100)
-        # computer_img = tkinter.PhotoImage(file="logo.png")
-        # canvas2.create_image(40, 40, image=computer_img)
-        # canvas2.grid(row=2, column=0)
-        # img_label = tkinter.Label(top, image=computer_img)
-        # img_label.grid(row=2, column=0)
-        #
-        # confirm_label = tkinter.Label(top, text=f"Website: {website}\n"
-        #                                         f"Email: {username}\n"
-        #                                         f"Password: {password}\n"
-        #                                         f"Is this ok?")
-        # confirm_label.grid(row=1, column=1, columnspan=2)
-        #
-        # def exit_top():
-        #     top.destroy()
-        #     top.update()
-        #
 
-        #
-        # # No button to exit the confirmation window
-        # no_button = tkinter.Button(top, text="No", command=exit_top)
-        # no_button.grid(row=2, column=2)
-        # # Yes button to continue recording password
-        # yes_button = tkinter.Button(top, text="Yes", command=record_password)
-        # yes_button.grid(row=2, column=3)
+
+# ------------------------------ Search ------------------------------- #
+def search():
+    website = entry_website.get()
+    # loading old data
+    with open("data.json", "r") as data_file:
+        if len(website) == 0:
+            messagebox.showinfo(title="Blank Input", message="Input Cannot Be Blank")
+        elif website in data_file:
+            data = json.load(data_file)
+            saved_username = data[website]["username"]
+            saved_password = data[website]["password"]
+            messagebox.showinfo(title="Password Information", message=f"username: {saved_username}\n"
+                                                                      f"password: {saved_password}")
+        else:
+            messagebox.showinfo(title="Password Information", message="No Data File Found")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -123,15 +109,15 @@ canvas.grid(row=0, column=1)
 label_website = tkinter.Label(text="Website")
 label_website.grid(row=1, column=0)
 # create entry box for website
-entry_website = tkinter.Entry(window, width=50)
-entry_website.grid(row=1, column=1, columnspan=2)
+entry_website = tkinter.Entry(window, width=31)
+entry_website.grid(row=1, column=1)
 
 # crate username label
 label_username = tkinter.Label(text="Email/Username")
 label_username.grid(row=2, column=0)
 # create entry box for username
-entry_username = tkinter.Entry(window, width=50)
-entry_username.grid(row=2, column=1, columnspan=2)
+entry_username = tkinter.Entry(window, width=48)
+entry_username.grid(row=2, column=1, columnspan=3)
 
 # crate password label
 label_password = tkinter.Label(text="Password")
@@ -142,11 +128,15 @@ entry_password.grid(row=3, column=1)
 password = generate_password()
 
 # create generate password button
-generate_button = tkinter.Button(text="Generate Password", width=15, command=lambda: entry_password.insert(0, password))
+generate_button = tkinter.Button(text="Create Password", width=13, command=lambda: entry_password.insert(0, password))
 generate_button.grid(row=3, column=2)
 
 # create add password button
 add_button = tkinter.Button(text="Add Password", width=42, command=add)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button.grid(row=4, column=1, columnspan=3)
+
+# add password search button
+search_button = tkinter.Button(text="Search", width=13, command=search)
+search_button.grid(row=1, column=2)
 
 tkinter.mainloop()
