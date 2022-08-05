380eff6579988c9a96734c78722f125eb7522c78
Ming Chen <mingchen0321@gmail.com>
2022-08-05 12:38:57 -0400

    add class code

--- Day33/main.py
+++ Day33/main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import requests
+from tkinter import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+MY_LAT = 43.159988
+MY_LNG = -79.247017
+
+# # get response from ISS API, it prints response code
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+# longitude = response.json()["iss_position"]["longitude"]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# ------------------------------------class challenge Below-------------------------------------
+# def get_quote():
+#     response = requests.get(url="https://api.kanye.rest")
+#     canvas.itemconfig(quote_text, text=response.json()["quote"])
+#
+# window = Tk()
+# window.title("Kanye Says...")
+# window.config(padx=50, pady=50)
+#
+# canvas = Canvas(width=300, height=414)
+# background_img = PhotoImage(file="background.png")
+# canvas.create_image(150, 207, image=background_img)
+# quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+# canvas.grid(row=0, column=0)
+#
+# kanye_img = PhotoImage(file="kanye.png")
+# kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+# kanye_button.grid(row=1, column=0)
+#
+# window.mainloop()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# ------------------------------------Sunset API example-------------------------------------
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0,
+}
+# this line of code will remove insecurerequestwarning error
+requests.packages.urllib3.disable_warnings()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters, verify=False)
+response.raise_for_status()
+requests.packages.urllib3.disable_warnings()
+data = response.json()["results"]["sunrise"]
+date, time = tuple(data.split("T"))
+print(time)
