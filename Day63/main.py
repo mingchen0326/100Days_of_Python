641e2ff76c60986b107efc9818964306584709cb
Ming Chen <mingchen0321@gmail.com>
2023-04-12 19:45:10 -0400

    update code for online library

--- Day63/main.py
+++ Day63/main.py
@@ -1,31 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+import sqlite3
 
+# create server
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
-db = SQLAlchemy(app)
 
+# create empty bookshelf
 all_books = []
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    author = db.Column(db.String(120), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
+# ------------------------------------------ Create Table -------------------------------------#
+# create database and cursor
+conn = sqlite3.connect("./instance/book_shelf.db", check_same_thread=False)
+c = conn.cursor()
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+# Check if table has been created
+try:
+    c.execute("""CREATE TABLE books(
+        book_title text,
+        book_author text,
+        book_rating text
+        )""")
+except sqlite3.OperationalError:
+    pass
 
-with app.app_context():
-    db.create_all()
-    books = db.session.execute(db.select(Book)).scalars()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # ------------------------------------------ Read Data ----------------------------------
+    all_books = c.execute("SELECT * FROM books")
+
+    # Commit our command
+    conn.commit()
+    return render_template('index.html', bookshelf=all_books)
 
 
 @app.route("/add", methods=['GET', 'POST'])
@@ -34,16 +39,20 @@ def add():
         book = {'book_name': request.form['book_name'],
                 'book_author': request.form['book_author'],
                 'book_rating': request.form['book_rating']}
-        new_book = Book(title=book['book_name'], author=book['book_author'], rating=book['book_rating'])
-        db.session.add(new_book)
-        db.session.commit()
 
-        # message = f"{book['book_name']} - {book['book_author']} - {book['book_rating']}/10"
-        # all_books = Book.query.all()
-        # print(all_books)
-        # all_books.append(book)
-        # return render_template('index.html', bookshelf=all_books)
+        # ------------------------------ Add single data and add many data ----------------------
+        c.execute(f"INSERT INTO books VALUES ('{book['book_name']}', '{book['book_author']}', '{book['book_rating']}')")
+
+        # ------------------------------------------ Read Data ----------------------------------
+        all_books = c.execute("SELECT * FROM books")
+
+        # Commit our command
+        conn.commit()
+        return render_template('index.html', bookshelf=all_books)
     return render_template('add.html')
+
+# # Close our connection
+# conn.close()
 
 
 if __name__ == "__main__":
